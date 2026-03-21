980f5a0ffe57e44b2914ed237d0824e96c1f119a
Atulkumar58 <atulchaudhary151@gmail.com>
2026-03-21 22:33:39 +0530

    cf practice

--- 1000/C_Raspberries.cpp
+++ 1000/C_Raspberries.cpp
@@ -13,7 +13,7 @@ int main(){
         for(int i=0; i<n; i++){
             cin>>a[i];
         }
-
+        
         int ans= INT_MAX;
         for(int i=0; i<n; i++){
             int t= a[i]%k;
@@ -22,6 +22,23 @@ int main(){
                 break;
             }
             ans= min(ans, k-t);
+        }
+
+        if(k==4 && ans!=0 && n>=2){
+            int c=0;
+            for(int i=0; i<n; i++){
+                if(a[i]%k==2){
+                    c++;
+                }
+            }
+            if(c>=2){
+                ans=0;
+            }else if(c==1){
+                ans=1;
+            }
+            else {
+                ans= min(ans, 2);
+            }
         }
         cout<<ans<<endl;
     }
