91951835952b68ea93eb4231bcd49597dc1b46ef
Atulkumar58 <atulchaudhary151@gmail.com>
2026-03-21 23:58:26 +0530

    Update B_Shoe_Shuffling.cpp

--- 1000/B_Shoe_Shuffling.cpp
+++ 1000/B_Shoe_Shuffling.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-using namespace std;
+#include <vector>
 
+using namespace std;
+void shift(vector<int> &ans, int l, int r){
+    for(int i=l; i<r; i++){
+        ans[i]= i+2;
+    }
+    ans[r]= l+1;
+}
 int main(){
     int t;
     cin>>t;
@@ -12,7 +19,36 @@ int main(){
 
         bool flag= 1;
         vector<int> ans(n);
+        int l=0;
+        for(int i=1; i<n; i++){
+            if(a[i] != a[i-1]){
+                if(l== i-1){
+                    flag= false;
+                    break;
+                }
+                else {
+                    shift(ans, l, i-1);
+                    l= i;
+                }
+            }
+        }
+        if(l == n-1){
+            flag= false;
+        }
+        else {
+            shift(ans, l, n-1);
+        }
         
+        if(flag){
+            for(int i=0; i<n; i++){
+                cout<<ans[i]<<" ";
+            }
+            cout<<"\n";
+        }
+        else {
+            cout<<"-1\n";
+        }
+      
     }
     return 0;
 }
