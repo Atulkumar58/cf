6c5d38692744aa6a4a7c9ae0968af63133866a6b
Atulkumar58 <atulchaudhary151@gmail.com>
2026-08-02 09:51:42 +0530

    Add solution for Codeforces problem B. Merge to Match

---
+++ contests/1-8-2026/B_Merge_to_Match.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < m; i++) cin >> b[i];
+
+    if (n < 2 * m) {
+        cout << "NO\n";
+        return;
+    }
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    bool possible = true;
+    for (int i = 0; i < m; i++) {
+        if (a[i] >= b[i] || a[n - m + i] <= b[i]) {
+            possible = false;
+            break;
+        }
+    }
+
+    if (possible) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
