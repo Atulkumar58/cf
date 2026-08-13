f85be51f5dd6ebb90951765b6eca45e4099511f9
Atulkumar58 <atulchaudhary151@gmail.com>
2026-08-13 17:42:37 +0530

    Refactor segment handling in C. Division and Union solution for clarity and efficiency

--- 1500/C_Division_and_Union.cpp
+++ 1500/C_Division_and_Union.cpp
@@ -14,41 +14,31 @@ int main(){
         for(int i=0; i<n; i++){
             int l,r;
             cin>>l>>r;
-            segment.push_back({l,r});
+            segment.push_back({l,r,i});
         }
         sort(segment.begin(), segment.end());
 
         vector<int> ans(n);
-        ans[0] = 1;
+        ans[segment[0][2]] = 1;
         int last[2] = {segment[0][1], -1};
         bool possible = false;
         for(int i=1; i<n; i++){
-            if(ans[i-1] == 1){
-                if(segment[i][0] > last[1]){
-                    ans[i]= 3-ans[i-1];
-                    last[1] = segment[i][1];
-                }
-                else{
-                    ans[i] = ans[i-1];
-                    last[0]= max(last[0], segment[i][1]);
-                }
+            int v=ans[segment[i-1][2]];
+            if(segment[i][0] <= segment[i-1][1] || last[v-1] >= segment[i][0]){
+                // Overlapping segments, assign the same group
+                ans[segment[i][2]] = v;
+                last[v-1] = max(last[v-1], segment[i][1]);
             }
             else{
-                if(segment[i][0] > last[0]){
-                    ans[i]= 3-ans[i-1];
-                    last[0] = segment[i][1]; 
-                }
-                else{
-                    ans[i]= ans[i-1];
-                    last[1]= max(last[1], segment[i][1]);
-                }
+                // Non-overlapping segments, assign a different group
+                v= 3-v;
+                ans[segment[i][2]] = v; // Toggle between 1 and 2
+                last[v-1] = max(last[v-1], segment[i][1]);
+                possible = true;
             }
-            
         }
-        // for(int i=0; i<n; i++)cout<<ans[i]<<" ";
-        // cout<<endl;
-
-        if(last[1] == -1){
+        
+        if(!possible){
             cout<<-1<<endl;
         }
         else{
